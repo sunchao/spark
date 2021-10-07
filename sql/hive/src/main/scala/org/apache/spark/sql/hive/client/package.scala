46d6927d301d446ebbf59269888eb0a53d276fce
Chao Sun <sunchao@apple.com>
2021-10-06 17:24:24 -0700

    Bump Hive version to 2.3.9.1-apple (#1223)

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/client/package.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/client/package.scala
@@ -101,7 +101,7 @@ package object client {
 
     // Since HIVE-23980, calcite-core included in Hive package jar.
     // For spark, only VersionsSuite currently creates a hive materialized view for testing.
-    case object v2_3 extends HiveVersion("2.3.9.0-apple",
+    case object v2_3 extends HiveVersion("2.3.9.1-apple",
       exclusions = Seq("org.apache.calcite:calcite-core",
         "org.apache.calcite:calcite-druid",
         "org.apache.calcite.avatica:avatica",
