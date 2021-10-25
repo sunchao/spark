be09e91267bd28f99b8bc7b25288a97652139de5
Huaxin Gao <huaxin_gao@apple.com>
2021-10-25 12:39:06 -0700

    rdar://84618469: use native parquet in predicate

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -844,7 +844,7 @@ object SQLConf {
       .internal()
       .intConf
       .checkValue(threshold => threshold >= 0, "The threshold must not be negative.")
-      .createWithDefault(10)
+      .createWithDefault(3000)
 
   val PARQUET_WRITE_LEGACY_FORMAT = buildConf("spark.sql.parquet.writeLegacyFormat")
     .doc("If true, data will be written in a way of Spark 1.4 and earlier. For example, decimal " +
