c8e5f7c4ff772b605896ed617011a046c333fb09
Chao Sun <sunchao@apache.org>
2022-05-23 13:25:10 -0700

    Revert "[SPARK-35378][SQL][FOLLOW-UP] Fix incorrect return type in CommandResultExec.executeCollect()"
    This reverts commit 5b479651dd782c54f87b5e9bf6c1b17bc4c0b0ab.

--- sql/core/src/test/scala/org/apache/spark/sql/execution/QueryExecutionSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/QueryExecutionSuite.scala
@@ -20,7 +20,6 @@ import scala.io.Source
 
 import org.apache.spark.sql.{AnalysisException, FastOperator}
 import org.apache.spark.sql.catalyst.analysis.UnresolvedNamespace
-import org.apache.spark.sql.catalyst.expressions.UnsafeRow
 import org.apache.spark.sql.catalyst.plans.QueryPlan
 import org.apache.spark.sql.catalyst.plans.logical.{CommandResult, LogicalPlan, OneRowRelation, Project, ShowTables, SubqueryAlias}
 import org.apache.spark.sql.catalyst.trees.TreeNodeTag
@@ -265,14 +264,6 @@ class QueryExecutionSuite extends SharedSparkSession {
     assert(cmdResultExec.commandPhysicalPlan.isInstanceOf[ExecutedCommandExec])
     assert(cmdResultExec.commandPhysicalPlan.asInstanceOf[ExecutedCommandExec]
       .cmd.isInstanceOf[ShowTablesCommand])
-  }
-
-  test("SPARK-35378: Return UnsafeRow in CommandResultExecCheck execute methods") {
-    val plan = spark.sql("SHOW FUNCTIONS").queryExecution.executedPlan
-    assert(plan.isInstanceOf[CommandResultExec])
-    plan.executeCollect().foreach { row => assert(row.isInstanceOf[UnsafeRow]) }
-    plan.executeTake(10).foreach { row => assert(row.isInstanceOf[UnsafeRow]) }
-    plan.executeTail(10).foreach { row => assert(row.isInstanceOf[UnsafeRow]) }
   }
 
   test("SPARK-38198: check specify maxFields when call toFile method") {
