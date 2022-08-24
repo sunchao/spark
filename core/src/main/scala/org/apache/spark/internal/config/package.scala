18702dff98220a12f279684416d2cb80df897608
Dongjoon Hyun <dhyun@apple.com>
2022-08-23 23:36:32 -0700

    [SPARK-40198][CORE] Enable `spark.storage.decommission.(rdd|shuffle)Blocks.enabled` by default (#1508)
    ### What changes were proposed in this pull request?
    
    rdar://99068608 (SPARK-40198 Enable `spark.storage.decommission.(rdd|shuffle)Blocks.enabled` by default)
    
    This PR aims to enable the following two configurations by default. Note that these storage configurations are disabled by default by the parent configuration, `spark.storage.decommission.enabled`, still. In addition, all decommission features including this storage decommissioning are disabled by `spark.decommission.enabled` by default.
    - `spark.storage.decommission.rddBlocks.enabled`
    - `spark.storage.decommission.shuffleBlocks.enabled`
    
    ### Why are the changes needed?
    
    This will help users use the storage decommissioning feature more easily.
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes, this will reduce the number of required configurations for decommission users.
    However, these two configurations are disabled by two configurations still.
    
    ### How was this patch tested?
    
    Pass the CIs.

--- core/src/main/scala/org/apache/spark/internal/config/package.scala
+++ core/src/main/scala/org/apache/spark/internal/config/package.scala
@@ -433,7 +433,7 @@ package object config {
         "a migratable shuffle resolver (like sort based shuffle)")
       .version("3.1.0")
       .booleanConf
-      .createWithDefault(false)
+      .createWithDefault(true)
 
   private[spark] val STORAGE_DECOMMISSION_SHUFFLE_MAX_THREADS =
     ConfigBuilder("spark.storage.decommission.shuffleBlocks.maxThreads")
@@ -448,7 +448,7 @@ package object config {
       .doc("Whether to transfer RDD blocks during block manager decommissioning.")
       .version("3.1.0")
       .booleanConf
-      .createWithDefault(false)
+      .createWithDefault(true)
 
   private[spark] val STORAGE_DECOMMISSION_MAX_REPLICATION_FAILURE_PER_BLOCK =
     ConfigBuilder("spark.storage.decommission.maxReplicationFailuresPerBlock")
