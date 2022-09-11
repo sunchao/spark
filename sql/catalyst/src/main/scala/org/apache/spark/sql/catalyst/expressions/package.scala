f86e318f2ab0f5bff5decaeedeaa493d4f2bdc53
Huaxin Gao <huaxin_gao@apple.com>
2022-09-11 12:20:31 -0700

    fix CI failure after cherry pick the upstream changes (#1558)
    * fix in predicate test failure
    
    * Revert "[SPARK-40149][SQL][3.2] Propagate metadata columns through Project"
    
    This reverts commit f78719f1f42fbaa1b82c81690c75dfb252d19afe.
    
    * Trigger Build

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/package.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/package.scala
@@ -23,7 +23,6 @@ import com.google.common.collect.Maps
 
 import org.apache.spark.sql.AnalysisException
 import org.apache.spark.sql.catalyst.analysis.{Resolver, UnresolvedAttribute}
-import org.apache.spark.sql.catalyst.util.MetadataColumnHelper
 import org.apache.spark.sql.types.{StructField, StructType}
 
 /**
@@ -266,7 +265,7 @@ package object expressions  {
         case (Seq(), _) =>
           val name = nameParts.head
           val attributes = collectMatches(name, direct.get(name.toLowerCase(Locale.ROOT)))
-          (attributes.filterNot(_.qualifiedAccessOnly), nameParts.tail)
+          (attributes, nameParts.tail)
         case _ => matches
       }
     }
@@ -315,12 +314,10 @@ package object expressions  {
       var i = nameParts.length - 1
       while (i >= 0 && candidates.isEmpty) {
         val name = nameParts(i)
-        val attrsToLookup = if (i == 0) {
-          direct.get(name.toLowerCase(Locale.ROOT)).map(_.filterNot(_.qualifiedAccessOnly))
-        } else {
-          direct.get(name.toLowerCase(Locale.ROOT))
-        }
-        candidates = collectMatches(name, nameParts.take(i), attrsToLookup)
+        candidates = collectMatches(
+          name,
+          nameParts.take(i),
+          direct.get(name.toLowerCase(Locale.ROOT)))
         if (candidates.nonEmpty) {
           nestedFields = nameParts.takeRight(nameParts.length - i - 1)
         }
