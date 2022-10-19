63c7e4e053a0fa9155fb3830152957f837a64e1d
Liang-Chi Hsieh <viirya@gmail.com>
2022-10-19 00:28:16 -0700

    MINOR: Update config name of CHECKPOINT_RENAMEDFILE_CHECK_ENABLED to match upstream

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -1598,8 +1598,9 @@ object SQLConf {
       .createWithDefault("lz4")
 
   val CHECKPOINT_RENAMEDFILE_CHECK_ENABLED =
-    buildConf("spark.sql.streaming.checkpoint.renamedFileCheck")
+    buildConf("spark.sql.streaming.checkpoint.renamedFileCheck.enabled")
       .doc("When true, Spark will validate if renamed checkpoint file exists.")
+      .version("3.4.0")
       .internal()
       .booleanConf
       .createWithDefault(false)
