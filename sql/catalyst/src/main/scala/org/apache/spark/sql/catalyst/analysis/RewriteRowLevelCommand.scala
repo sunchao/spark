774f8b14764f9039802695316a2df5044c0074ff
Anton Okolnychyi <aokolnychyi@apple.com>
2022-10-23 22:32:55 -0700

    rdar://101429519 (Fix nullability propagation in MERGE) (#1600)
    ### What changes were proposed in this pull request?
    
    This PR cherry-picks Iceberg fixes to our row-level implementation.
    https://github.com/apache/iceberg/commit/3bc2f97c41c40aba738289b0083ced13540b6adb
    https://github.com/apache/iceberg/commit/d5411d2c7f6fe0c35f33c22465c74971b0481b9d
    
    ### Why are the changes needed?
    
    These changes are needed to respect attribute nullability in MERGE.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    
    ### How was this patch tested?
    
    Iceberg tests.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/RewriteRowLevelCommand.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/RewriteRowLevelCommand.scala
@@ -71,20 +71,15 @@ trait RewriteRowLevelCommand extends Rule[LogicalPlan] with IcebergSupport {
       metadataAttrs: Seq[Attribute]): WriteDeltaProjections = {
 
     val rowProjection = if (rowAttrs.nonEmpty) {
-      Some(newLazyProjection(plan, rowAttrs, usePlanTypes = true))
+      Some(newLazyProjection(plan, rowAttrs))
     } else {
       None
     }
 
-    // in MERGE, the plan may contain both delete and insert records that may affect
-    // the nullability of metadata columns (e.g. metadata columns for new records are always null)
-    // since metadata columns are never passed with new records to insert,
-    // use the actual metadata column types instead of the ones present in the plan
-
-    val rowIdProjection = newLazyProjection(plan, rowIdAttrs, usePlanTypes = false)
+    val rowIdProjection = newLazyProjection(plan, rowIdAttrs)
 
     val metadataProjection = if (metadataAttrs.nonEmpty) {
-      Some(newLazyProjection(plan, metadataAttrs, usePlanTypes = false))
+      Some(newLazyProjection(plan, metadataAttrs))
     } else {
       None
     }
@@ -95,17 +90,11 @@ trait RewriteRowLevelCommand extends Rule[LogicalPlan] with IcebergSupport {
   // the projection is done by name, ignoring expr IDs
   private def newLazyProjection(
       plan: LogicalPlan,
-      attrs: Seq[Attribute],
-      usePlanTypes: Boolean): ProjectingInternalRow = {
+      projectedAttrs: Seq[Attribute]): ProjectingInternalRow = {
 
-    val colOrdinals = attrs.map(attr => plan.output.indexWhere(_.name == attr.name))
-    val schema = if (usePlanTypes) {
-      val planAttrs = colOrdinals.map(plan.output(_))
-      StructType.fromAttributes(planAttrs)
-    } else {
-      StructType.fromAttributes(attrs)
-    }
-    ProjectingInternalRow(schema, colOrdinals)
+    val projectedOrdinals = projectedAttrs.map(attr => plan.output.indexWhere(_.name == attr.name))
+    val schema = StructType.fromAttributes(projectedOrdinals.map(plan.output(_)))
+    ProjectingInternalRow(schema, projectedOrdinals)
   }
 
   protected def resolveRequiredMetadataAttrs(
