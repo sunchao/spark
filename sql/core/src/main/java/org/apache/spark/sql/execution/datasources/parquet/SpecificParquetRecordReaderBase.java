55bfff4325466a4820263eb93677c491c5da862f
alfreddavidson <alfie.davidson9@gmail.com>
2023-02-08 11:07:59 +0900

    [SPARK-40819][SQL][3.2] Timestamp nanos behaviour regression
    As per HyukjinKwon request on https://github.com/apache/spark/pull/38312 to backport fix into 3.2
    ### What changes were proposed in this pull request?
    
    Handle `TimeUnit.NANOS` for parquet `Timestamps` addressing a regression in behaviour since 3.2
    
    ### Why are the changes needed?
    
    Since version 3.2 reading parquet files that contain attributes with type `TIMESTAMP(NANOS,true)` is not possible as ParquetSchemaConverter returns
    ```
    Caused by: org.apache.spark.sql.AnalysisException: Illegal Parquet type: INT64 (TIMESTAMP(NANOS,true))
    ```
    https://issues.apache.org/jira/browse/SPARK-34661 introduced a change matching on the `LogicalTypeAnnotation` which only covers Timestamp cases for `TimeUnit.MILLIS` and `TimeUnit.MICROS` meaning `TimeUnit.NANOS` would return `illegalType()`
    
    Prior to 3.2 the matching used the `originalType` which for `TIMESTAMP(NANOS,true)` return `null` and therefore resulted to a `LongType`, the change proposed is too consider `TimeUnit.NANOS` and return `LongType` making behaviour the same as before.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No
    
    ### How was this patch tested?
    
    Added unit test covering this scenario.
    Internally deployed to read parquet files that contain `TIMESTAMP(NANOS,true)`
    
    Closes #39905 from awdavidson/ts-nanos-fix-3.2.
    
    Authored-by: alfreddavidson <alfie.davidson9@gmail.com>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- sql/core/src/main/java/org/apache/spark/sql/execution/datasources/parquet/SpecificParquetRecordReaderBase.java
+++ sql/core/src/main/java/org/apache/spark/sql/execution/datasources/parquet/SpecificParquetRecordReaderBase.java
@@ -161,6 +161,7 @@ public abstract class SpecificParquetRecordReaderBase<T> extends RecordReader<Vo
     Configuration config = new Configuration();
     config.setBoolean(SQLConf.PARQUET_BINARY_AS_STRING().key() , false);
     config.setBoolean(SQLConf.PARQUET_INT96_AS_TIMESTAMP().key(), false);
+    config.setBoolean(SQLConf.LEGACY_PARQUET_NANOS_AS_LONG().key(), false);
 
     Path file = new Path(path);
     long length = file.getFileSystem(config).getFileStatus(file).getLen();
